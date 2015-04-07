9ee1b274cc55edeace118134f1d219469d534878
Andrew Wong <arwong09@gmail.com>
2015-04-07 10:22:00 -0700

    mvp

---
+++ src/titleist.js
@@ -0,0 +1,75 @@
+define(["react"], function(React) {
+
+  var extend = function(target, source) {
+    Object.keys(source).forEach(function(key) {
+      target[key] = source[key];
+    });
+  };
+
+  var options = {
+    defaultText: '',
+    defaultStopWords: ['a','an','and','as','at','but','by','for','from','in','into','nor','of','on','onto','or','the','to','vs','with','vs.'],
+    stopWords: [],
+    componentDidMount: function() {
+      this.stopWords = this.stopWords.concat(this.defaultStopWords);
+    },
+    getInitialState: function() {
+      return {value: this.defaultText, checked: true};
+    },
+    handleChange: function() {
+      var text = this.refs.input.getDOMNode().value;
+
+      if(this.state.checked === false) {
+        return this.setState({value: text});
+      }
+
+      if(text[text.length - 1] === " ") {
+        this.capitalizeLastWord(text);
+      } else {
+        this.setState({value: text});
+      }
+    },
+    capitalizeLastWord: function(text) {
+      var wordArray, lastWord, modifiedWord, newText;
+
+      wordArray = text.split(" ")
+      lastWord = wordArray[wordArray.length - 2];
+
+      if(this.stopWords.indexOf(lastWord) === -1) {
+        modifiedWord = this.capitalize(lastWord);
+      }
+
+      wordArray[wordArray.length - 2] = modifiedWord || lastWord;
+      newText = wordArray.join(" ");
+      this.setState({value: newText});
+    },
+    toggleChecked: function() {
+      this.setState({checked: !this.state.checked});
+      React.findDOMNode(this.refs.input).focus();
+    },
+    capitalize: function(string) {
+      return string.replace(/^([a-z])|\s([a-z])/g, function(char) { return char.toUpperCase() });
+    },
+    render: function() {
+      return (
+        React.createElement("div", null, 
+          React.createElement("input", {type: "text", name: "title", id: "titleist-input", onChange: this.handleChange, ref: "input", value: this.state.value}), 
+          React.createElement("label", {className: "titleist-toggle-label"}, 
+            React.createElement("i", {className: "fa fa-bolt toggled-" + this.state.checked}), 
+            React.createElement("input", {type: "checkbox", id: "titleist-toggle", ref: "checkbox", onChange: this.toggleChecked, checked: this.state.checked})
+          )
+        )
+      )
+    }
+  }
+
+  var Titleist = {
+    initialize: function(selector, customOptions) {
+      extend(options, customOptions);
+      var TitleistComponent = React.createClass(options);
+      React.render(React.createElement(TitleistComponent, null), document.getElementById(selector));
+    }
+  }
+
+  return Titleist;
+});
